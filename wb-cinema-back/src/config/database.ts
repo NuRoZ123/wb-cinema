9992e3ca0e06c6afc88025cecdcd64a40eb41b56
NuRoZ123 <killiangouzien44220@gmail.com>
2025-02-05 12:36:05 +0100

    FIX: #10: deploiement du back sur le server en ssh

--- wb-cinema-back/src/config/database.ts
+++ wb-cinema-back/src/config/database.ts
@@ -4,13 +4,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-console.log("Database config:", {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-});
 export const AppDataSource = new DataSource({
   type: "mysql",
   host: process.env.DB_HOST,
