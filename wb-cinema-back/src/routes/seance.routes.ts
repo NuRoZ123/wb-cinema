93b7f488348c130c7521158cbabbf2a99a038670
Alan <alan.jordy1999@gmail.com>
2025-02-06 09:30:31 +0100

    Feat #15 : delete seance

--- wb-cinema-back/src/routes/seance.routes.ts
+++ wb-cinema-back/src/routes/seance.routes.ts
@@ -2,6 +2,7 @@ import express from "express";
 import {
   getSeancesByFilmId,
   createSeance,
+  deleteSeance,
 } from "../controllers/seance.controller";
 import { validate } from "../middlewares/validate.middleware";
 import { seanceSchema } from "../validation/seance.validation";
@@ -9,5 +10,6 @@ import { seanceSchema } from "../validation/seance.validation";
 const router = express.Router();
 router.get("/:id", getSeancesByFilmId);
 router.post("/", validate(seanceSchema), createSeance);
+router.delete("/:id", deleteSeance);
 
 export default router;
